b46d48562f5eb69b5dc93dbff66a88329b821782
cgorrino <cgorrino03@gmail.com>
2021-04-06 22:18:51 -0700

    beginning to add timeblocks with appropriate styles.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+var inputEls = document.querySelectorAll(".event-input");
 // Set date on header with the right format
-var currentDate = moment().format("dddd, MMMM Do, YYYY")
-$("#currentDay").text(currentDate);
+function displayDate() {
+    var currentDate = moment().format("dddd, MMMM Do, YYYY")
+    $("#currentDay").text(currentDate);
+}
+displayDate();
+
+// Figure out the present hour, past, future
+// Assign classes based on this
+console.log(inputEls);
+
+var currentHour = moment().format("hh a");
+$("#currentHour").text(currentHour);
+
+// Use moment to compare the "hour" text with calculated hour
+
+// Save inputs and persist to local storage
+
+// Listener for save buttons
