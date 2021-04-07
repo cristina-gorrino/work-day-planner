bb9b4eb2733913c2b400fae47302c1b0c0dfbb87
cgorrino <cgorrino03@gmail.com>
2021-04-07 00:14:35 -0700

    Cleaning up print statements and creating a function to style hour sections.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var inputEls  = document.querySelectorAll("input");
-var hourEls = document.querySelectorAll(".hour");
+
 // Set date on header with the right format
 function displayDate() {
     var currentDate = moment().format("dddd, MMMM Do, YYYY")
@@ -7,29 +7,25 @@ function displayDate() {
 }
 displayDate();
 
-// Figure out the present hour, past, future
-// Assign classes based on this
+
 // Iterate over each of the hours in the planner
 // Compare with current hour to assign classes that correspond to past/ present/ future
-$.each(hourEls, function(i, hourEls){
-    console.log(`i ${i}`);
-    console.log(`hourEls.textContent: ${hourEls.textContent}`);
-    if (moment().format("hh a") === hourEls.textContent) {
-        console.log("match hour")
-        console.log(hourEls.nextElementSibling.classList);
-        hourEls.nextElementSibling.className = "present";
-        console.log(hourEls.nextElementSibling.classList);
-    } else if (moment().format("hh a") > hourEls.textContent) {
-        console.log("greater hour");
-        hourEls.nextElementSibling.className = "past";
-    } else if (moment().format("hh a") < hourEls.textContent) {
-        console.log("lesser hour");
-        hourEls.nextElementSibling.className = "future";
-    }
-})
+function styleHourSections(){
+    var hourEls = document.querySelectorAll(".hour");
+    $.each(hourEls, function(i, hourEls){
+        if (moment().format("hh a") === hourEls.textContent) {
+            hourEls.nextElementSibling.className = "present";
+        } else if (moment().format("hh a") > hourEls.textContent) {
+            hourEls.nextElementSibling.className = "past";
+        } else if (moment().format("hh a") < hourEls.textContent) {
+            hourEls.nextElementSibling.className = "future";
+        }
+    });
+}
+styleHourSections();
 
-var currentHour = moment().format("hh a");
-$("#currentHour").text(currentHour);
+
+
 
 // Use moment to compare the "hour" text with calculated hour
 
