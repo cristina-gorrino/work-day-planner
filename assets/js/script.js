0d9d4bc266e7b021e6a41c588b443009ae1c6c59
cgorrino <cgorrino03@gmail.com>
2021-04-07 00:07:05 -0700

    Adding the logic to determine past present and future hours in the table.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var inputEls = document.querySelectorAll(".event-input");
+var hourEls = document.querySelectorAll(".hour");
 // Set date on header with the right format
 function displayDate() {
     var currentDate = moment().format("dddd, MMMM Do, YYYY")
@@ -9,6 +10,24 @@ displayDate();
 // Figure out the present hour, past, future
 // Assign classes based on this
 console.log(inputEls);
+// Iterate over each of the hours in the planner
+// Compare with current hour to assign classes that correspond to past/ present/ future
+$.each(hourEls, function(i, hourEls){
+    console.log(`i ${i}`);
+    console.log(`hourEls.textContent: ${hourEls.textContent}`);
+    if (moment().format("hh a") === hourEls.textContent) {
+        console.log("match hour")
+        console.log(hourEls.nextElementSibling.classList);
+        hourEls.nextElementSibling.className = "present";
+        console.log(hourEls.nextElementSibling.classList);
+    } else if (moment().format("hh a") > hourEls.textContent) {
+        console.log("greater hour");
+        hourEls.nextElementSibling.className = "past";
+    } else if (moment().format("hh a") < hourEls.textContent) {
+        console.log("lesser hour");
+        hourEls.nextElementSibling.className = "future";
+    }
+})
 
 var currentHour = moment().format("hh a");
 $("#currentHour").text(currentHour);
